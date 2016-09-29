f1e4ec6f045931a2f3e6e32c2ad7fa069a5d6254
luis jose sanchez carrasco <luissancar@yahoo.es>
2016-09-29 18:04:54 +0200

    29 septiembre

--- app/src/main/java/com/example/luissancar/morcilla/MainActivity.java
+++ app/src/main/java/com/example/luissancar/morcilla/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.luissancar.morcilla;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,15 +8,18 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WebView w;
 
     EditText url;
+    TextView footer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         w=(WebView)findViewById(R.id.web);
         w.setWebViewClient(new WebViewClient());  // al hacer click no salta a otro navegador
         url=(EditText)findViewById(R.id.editText2);
+        footer=(TextView)findViewById(R.id.textView2);
 
 
         final EditText edittext = (EditText) findViewById(R.id.editText2);
@@ -50,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
 
          //Bundle bun=getIntent().getExtras();
          w.loadUrl("http://"+url.getText().toString());
+         footer.setText(w.getUrl().toString());
+         //Lineas para ocultar el teclado virtual (Hide keyboard)
+         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+         imm.hideSoftInputFromWindow(url.getWindowToken(), 0);
      }
 
      public void gooBack(View v) {
