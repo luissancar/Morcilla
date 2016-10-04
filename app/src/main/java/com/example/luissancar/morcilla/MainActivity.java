d385b8c74589dd6727fda31474d3bc2590658108
luissancar <luissancar@yahoo.es>
2016-10-04 11:52:36 +0200

    lans ds

--- app/src/main/java/com/example/luissancar/morcilla/MainActivity.java
+++ app/src/main/java/com/example/luissancar/morcilla/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.luissancar.morcilla;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -18,22 +19,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
     WebView w;
 
     EditText url;
     TextView footer;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-
-        w=(WebView)findViewById(R.id.web);
+        w = (WebView) findViewById(R.id.web);
         w.setWebViewClient(new WebViewClient());  // al hacer click no salta a otro navegador
-        url=(EditText)findViewById(R.id.editText2);
-        footer=(TextView)findViewById(R.id.textView2);
+        url = (EditText) findViewById(R.id.editText2);
+        footer = (TextView) findViewById(R.id.textView2);
 
 
         final EditText edittext = (EditText) findViewById(R.id.editText2);
@@ -50,39 +61,94 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-        if (savedInstanceState!=null) {
-            url.setText(savedInstanceState.getString("url"));
-            goWeb();
-        }
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        if (outState!=null) {
-            String url2=new  String();
-            outState.putString("url", (String) url2 );
-            url.setText(url2.toString());
-            super.onSaveInstanceState(outState);}
+    protected void onSaveInstanceState(Bundle instanceState)
+    {
+        super.onSaveInstanceState(instanceState);
+
+        instanceState.putString("uuu", url.toString()); // increment counter here
     }
+
+
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+
+        super.onRestoreInstanceState(savedInstanceState);
+        String url2 = savedInstanceState.getString("uuu");
+        url.setText(url2.toString());
+       // goWeb();
+    }
+
+
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+//
+// Do your work here...
+//
+    }
+
 
     public void goWeb() {
         if (!url.getText().toString().startsWith("http://"))
             url.setText("http://" + url.getText().toString());
-         if (URLUtil.isNetworkUrl(url.getText().toString()) ) {
-             footer.setText("Esperando a: " + url.getText().toString());
-             w.loadUrl(url.getText().toString());
-             footer.setText(url.getText().toString());
-         }
-         //Lineas para ocultar el teclado virtual (Hide keyboard)
-         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-         imm.hideSoftInputFromWindow(url.getWindowToken(), 0);
-     }
-
-     public void gooBack(View v) {
-
-         if (w.canGoBack())
-             w.goBack();
+        if (URLUtil.isNetworkUrl(url.getText().toString())) {
+            footer.setText("Esperando a: " + url.getText().toString());
+            w.loadUrl(url.getText().toString());
+            footer.setText(url.getText().toString());
+        }
+        //Lineas para ocultar el teclado virtual (Hide keyboard)
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(url.getWindowToken(), 0);
     }
 
+    public void gooBack(View v) {
+
+        if (w.canGoBack())
+            w.goBack();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
